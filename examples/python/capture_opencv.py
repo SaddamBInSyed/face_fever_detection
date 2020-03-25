Major updates: - add NUC - fix bug in forehead temprature calculation - fix bug in mouse_crop.py - imporve display

examples/python/capture_opencv.py
@@ -6,10 +6,11 @@ import os
 import datetime
 import argparse
 import glob
-sys.path.insert(0,'../RetinaFace')
+
+sys.path.insert(0, '../RetinaFace')
 from retinaface import RetinaFace
 import time
-import  mouse_crop
+import mouse_crop
 import numpy as np
 
 import cv2
@@ -18,13 +19,21 @@ from time import sleep
 
 FACE_DETECTION_THRESH = 0.2
 TEMPERATURE_THRESH = 37.5
-FACE_EMISSIVITY = 0.92#0.98
-BLACK_BODY_EMISSIVITY=1.0
-BLACK_BODY_TEMP=[36.0, 39.0]
-A=0.01176
-B=-2.0
+FACE_EMISSIVITY = 0.92  # 0.98
+BLACK_BODY_EMISSIVITY = 1.0
+BLACK_BODY_TEMP = [36.0, 38.0]
+A = 0.01176
+B = -2.0
 
-USE_BLACK_BODY =True
+l_ratio = 0.25
+r_ratio = 0.75
+t_ratio = 0.1
+b_ratio = 0.3  # 0.35
+
+colormap = None
+# colormap = 2 # JET
+
+USE_BLACK_BODY = True
 scales = [1024, 1980]
 
 count = 1
@@ -38,19 +47,18 @@ if PY_MAJOR_VERSION > 2:
 else:
     NULL_CHAR = '\0'
 
-def main():
-   
 
+def main():
     # Instantiate the parser
     parser = argparse.ArgumentParser()
 
     parser.add_argument('name', type=str,
                         help='part of shared memory name')
 
-    parser.add_argument('-noshow', action='store_true', default = False,
+    parser.add_argument('-noshow', action='store_true', default=False,
                         help='Specify -nowshow flag if you don\'t want to show the images on the screen')
-    
-    parser.add_argument('-d', action='store_true', default = False,
+
+    parser.add_argument('-d', action='store_true', default=False,
                         help='Specify to dump the incoming frames to files')
 
     parser.add_argument('-o', type=str, default="/media/nano/Elements/",
@@ -67,9 +75,14 @@ def main():
     g_dumpFiles = False
     g_log2Length = args.l
     g_showPic = not args.noshow
-    g_dumpPath = '/media/481D35AA4108CBB0/fever_det/'
-    g_drawGraph = True # showing the pic implies not drawing the graph
-    
+    g_dumpPath = '/home/nvidia/Documents/fever_det/'
+    g_drawGraph = True  # showing the pic implies not drawing the graph
+
+    # get image pattern for NUC
+    subtract_image_pattern = True
+    dirname = r'/home/nvidia/Documents/fever_det/2020_25_03__17_35_50'
+    img_mean, img_pattern_mean_subtracted = calc_image_pattern(dirname)
+
     print("Params:")
     print("name:          {}".format(name))
     print("Dump files:    {}".format(g_dumpFiles))
@@ -79,9 +92,11 @@ def main():
         try:
             os.mkdir(g_dumpPath)
         except:
-            import traceback; traceback.print_exc()
+            import traceback;
+            traceback.print_exc()
             print("ERROR: can not create folder: {}".format(g_dumpPath))
-            import pdb;pdb.set_trace()
+            import pdb;
+            pdb.set_trace()
     print("Show image:    {}".format(g_showPic))
 
     g_log2Length = 2
@@ -99,16 +114,15 @@ def main():
     nOutputImageQ = 1 << g_log2Length
     print("nOutputImageQ: {} ".format(nOutputImageQ))
 
+    thermapp = pyipccap.thermapp(None, nOutputImageQ)
+    # thermapp = pyipccap.thermapp(name,nOutputImageQ)
 
-    thermapp = pyipccap.thermapp(None,nOutputImageQ)
-    #thermapp = pyipccap.thermapp(name,nOutputImageQ)
-    
     print "Open shared memory..."
 
     thermapp.open_shared_memory()
 
     print "Shared memory opened"
-    
+
     rate_list = []
     frame_count = 0
     prev = 0
@@ -120,9 +134,9 @@ def main():
         while True:
 
             data = thermapp.get_data()
-            if data is None :
+            if data is None:
                 continue
-        
+
             # We have new data
 
             # time calc
@@ -130,24 +144,30 @@ def main():
             diff = now - last_msg
             last_msg = now
             if diff > 0:
-                msg_rate = 1.0/diff
+                msg_rate = 1.0 / diff
             else:
                 msg_rate = 0.0
             miss = thermapp.imageId - prev - 1
             prev = thermapp.imageId
-        
 
-            avg_max = 60 # count avg every avg_max frames
+            avg_max = 60  # count avg every avg_max frames
             if avg_count < avg_max:
                 avg_accum += msg_rate
                 avg_count += 1
-                print("Got {}, dim ({},{}), missed = {}, rate = {} Hz".format(thermapp.imageId, thermapp.imageWidth, thermapp.imageHeight, miss, round(msg_rate,2)))
+                print("Got {}, dim ({},{}), missed = {}, rate = {} Hz".format(thermapp.imageId, thermapp.imageWidth,
+                                                                              thermapp.imageHeight, miss,
+                                                                              round(msg_rate, 2)))
             else:
-                avg_rate = avg_accum/avg_max
-                print("Got {}, dim ({},{}), missed = {}, rate = {} Hz, avg_rate = {} Hz".format(thermapp.imageId, thermapp.imageWidth, thermapp.imageHeight, miss, round(msg_rate,2), round(avg_rate,2)))
+                avg_rate = avg_accum / avg_max
+                print("Got {}, dim ({},{}), missed = {}, rate = {} Hz, avg_rate = {} Hz".format(thermapp.imageId,
+                                                                                                thermapp.imageWidth,
+                                                                                                thermapp.imageHeight,
+                                                                                                miss,
+                                                                                                round(msg_rate, 2),
+                                                                                                round(avg_rate, 2)))
                 avg_accum = 0
                 avg_count = 0
-            
+
             if (frame_count) > 10:
                 rate_list.append(msg_rate)
                 total_misses += miss
@@ -156,24 +176,27 @@ def main():
                 font = cv2.FONT_HERSHEY_PLAIN
                 img_st = data[64:]
                 rgb = np.frombuffer(img_st, dtype=np.uint16).reshape(thermapp.imageHeight, thermapp.imageWidth)
+                if subtract_image_pattern:
+                    rgb = rgb - img_pattern_mean_subtracted
                 # rgb = np.rot90(rgb,3)
-                #rgb = np.uint8(rgb)
+                # rgb = np.uint8(rgb)
+
                 rgb = cv2.merge((rgb, rgb, rgb))
-                im_raw =rgb.copy()
+                im_raw = rgb.copy()
                 im = rgb - rgb.min()
                 im = im.astype('float')
-                im = (im/(im.max())*255).astype('uint8')
+                im = (im / (im.max()) * 255).astype('uint8')
 
                 if g_calib_black_body:
-                    A,B = calib_black_body(im=im, im_raw=im_raw,num_of_body=2)
-                    g_calib_black_body =False
+                    A, B, coor = calib_black_body(im=im, im_raw=im_raw, num_of_body=2)
+                    g_calib_black_body = False
 
                 scales = [1024, 1980]
 
-
-                im_plot = cv2.resize(im,(384*2,288*2))
+                im_plot = cv2.resize(im, (384 * 2, 288 * 2))
                 # im_plot = im
-                im_plot = cv2.applyColorMap(im_plot,2) # 2 is type of color map
+                if colormap is not None:
+                    im_plot = cv2.applyColorMap(im_plot, colormap)  # 2 is type of color map
 
                 print(im.shape)
                 im_shape = im.shape
@@ -197,41 +220,59 @@ def main():
                 print(faces.shape, landmarks.shape, end_t - start_t)
 
                 # detect black body
-                # top, left, bottom, right, rect_est = detect_rectangle(im_raw.astype('uint8'))
-                top, left, bottom, right, rect_est = detect_rectangle(im)
-
+                # top, left, bottom, right, rect_est = detect_rectangle(im_raw.astype('uint8')).
+                rect_est = None
+                # top, left, bottom, right, rect_est = detect_rectangle(im)
 
                 if rect_est is not None:
                     dh = bottom - top
                     dw = right - left
                     hh = 0.1
-                    #cv2.drawContours(im_plot, 2*[rect_est], 0, (255, 255, 0), 2)
-                    ref_temp_raw = np.mean(im_raw[int(top+hh*dh):int(bottom-hh*dh), int(left+hh*dw):int(right-hh*dw), 0])
+                    # cv2.drawContours(im_plot, 2*[rect_est], 0, (255, 255, 0), 2)
+                    ref_temp_raw = np.mean(
+                        im_raw[int(top + hh * dh):int(bottom - hh * dh), int(left + hh * dw):int(right - hh * dw), 0])
                     print('black body found!, {}'.format(ref_temp_raw))
                     status_text = 'black body found'
                     color = (0, 255, 0)
-                    cv2.rectangle(im_plot, (2 * int(left+hh*dw), 2 * int(top+hh*dh) ), (2 * int(right-hh*dw), 2 * int(bottom-hh*dh)), color, 3)
+                    cv2.rectangle(im_plot, (2 * int(left + hh * dw), 2 * int(top + hh * dh)),
+                                  (2 * int(right - hh * dw), 2 * int(bottom - hh * dh)), color, 3)
                     cv2.rectangle(im_plot, (2 * left, 2 * top), (2 * right, 2 * bottom), color, 3)
 
 
                 else:
-                    ref_temp_raw = np.median(np.sort(im_raw[:, :, 0].reshape([-1]))[-100:])
-                    #ref_temp_raw = np.max(np.sort(im_raw[:, :, 0].reshape([-1]))[-100:])
+                    color = (255, 0, 0)
+                    left, top, right, bottom = coor[0]
+                    ref_temp_raw = np.median(im_raw[top:bottom, left:right, 0])
+                    cv2.rectangle(im_plot, (2 * left, 2 * top), (2 * right, 2 * bottom), color, 3)
+                    ref_temp = (ref_temp_raw - B) / (BLACK_BODY_EMISSIVITY * A)
+                    color = (0, 255, 0)
+                    cv2.putText(im_plot, '{}'.format(np.round(ref_temp, 1)), (2 * left, 2 * top - 20), font, 2, color,
+                                3)
+
+                    color = (255, 0, 0)
+                    left, top, right, bottom = coor[1]
+                    ref_temp_raw = np.median(im_raw[top:bottom, left:right, 0])
+                    cv2.rectangle(im_plot, (2 * left, 2 * top), (2 * right, 2 * bottom), color, 3)
+                    ref_temp = (ref_temp_raw - B) / (BLACK_BODY_EMISSIVITY * A)
+                    color = (0, 255, 0)
+                    cv2.putText(im_plot, '{}'.format(np.round(ref_temp, 1)), (2 * left, 2 * top - 20), font, 2, color,
+                                3)
+
+                    # ref_temp_raw = np.max(np.sort(im_raw[:, :, 0].reshape([-1]))[-100:])
                     # ref_temp_raw = im_raw.max()
                     # ref_temp = (ref_temp_raw - 14336) * 0.00652 + 110.7
                     print('black body not found :(')
                     status_text = 'black body not found'
-                    color = (0, 0, 255)
+                    # ref_temp = (ref_temp_raw - B) / (BLACK_BODY_EMISSIVITY * A)
+                    # cv2.putText(im_plot, '{}'.format(ref_temp), (2 * left, 2 * top - 20), font, 2, color, 3)
 
-                ref_temp = ref_temp_raw * A +B
                 print(ref_temp_raw)
                 print(ref_temp)
 
+                # cv2.rectangle(im_plot, (2 * left, 2 * top), (2 * right, 2 * bottom), color, 3)
+                # )
 
-                #cv2.rectangle(im_plot, (2 * left, 2 * top), (2 * right, 2 * bottom), color, 3)
-                # cv2.putText(im_plot, 'ref', (2 * left, 2 * top), (2 * right, 2 * bottom - 20), font, 2, color, 3)
-
-                cv2.putText(im_plot, status_text, (10,10), font, 3,
+                cv2.putText(im_plot, status_text, (10, 10), font, 3,
                             color, 3)
 
                 if faces is not None and len(faces) > 0:
@@ -243,46 +284,53 @@ def main():
                         color = (0, 0, 255)
                         # temp_raw = im_raw[box[0]:box[2],box[1]:box[3],0].mean()
                         box_draw = 2 * box
-                        top = box_draw[1]
-                        left = box_draw[0]
 
-                        w = box_draw[2] - box_draw[0]
-                        h = box_draw[3] - box_draw[1]
+                        top_draw = box_draw[1]
+                        left_draw = box_draw[0]
 
-                        l_ratio = 0.25
-                        r_ratio = 0.75
-                        t_ratio = 0.1
-                        b_ratio = 0.35
+                        w_draw = box_draw[2] - box_draw[0]
+                        h_draw = box_draw[3] - box_draw[1]
+
+                        hor_start_draw = int(left_draw + l_ratio * w_draw)
+                        hor_end_draw = int(left_draw + r_ratio * w_draw)
+                        ver_start_draw = int(top_draw + t_ratio * h_draw)
+                        ver_end_draw = int(top_draw + b_ratio * h_draw)
+
+                        cv2.rectangle(im_plot, (hor_start_draw, ver_start_draw), (hor_end_draw, ver_end_draw),
+                                      (255, 255, 255), 2)
+
+                        # for temparture calculation - use raw image
+                        top = box[1]
+                        left = box[0]
+
+                        w = box[2] - box[0]
+                        h = box[3] - box[1]
 
                         hor_start = int(left + l_ratio * w)
                         hor_end = int(left + r_ratio * w)
                         ver_start = int(top + t_ratio * h)
                         ver_end = int(top + b_ratio * h)
 
-
-                        cv2.rectangle(im_plot, (hor_start, ver_start), (hor_end, ver_end), (255, 255, 255), 2)
-
-
-
-                        temp_raw =np.median(im_raw[ver_start:ver_end,hor_start:hor_end,0])
+                        temp_raw = np.median(im_raw[ver_start:ver_end, hor_start:hor_end, 0])
                         # a= 1.0/(FACE_EMISSIVITY*A)
                         # b= B*FACE_EMISSIVITY*a
                         # temp_raw = temp_raw/FACE_EMISSIVITY
-                        temp =(temp_raw-B)/(FACE_EMISSIVITY*A)
+                        temp = (temp_raw - B) / (FACE_EMISSIVITY * A)
+                        # temp =(temp_raw-B)/(BLACK_BODY_EMISSIVITY*A) # FIXME!!!!
 
                         # temp = (temp_raw - 14336) * 0.00652 +110.7
 
-                        is_temp_ok = temp_raw <= ref_temp_raw*FACE_EMISSIVITY
+                        is_temp_ok = temp_raw <= ref_temp_raw * FACE_EMISSIVITY
+
+                        text_ = np.round(temp, 1)
 
                         if is_temp_ok:
-                            color = (0,255 ,0)
-                            text_ = temp
+                            color = (0, 255, 0)
                         else:
-                            color = (0,0,255)
-                            text_ = temp
+                            color = (0, 0, 255)
                         cv2.rectangle(im_plot, (box_draw[0], box_draw[1]), (box_draw[2], box_draw[3]), color, 3)
 
-                        cv2.putText(im_plot,'{}'.format(text_), (box_draw[0], box_draw[1] - 20), font, 2,
+                        cv2.putText(im_plot, '{}'.format(text_), (box_draw[0], box_draw[1] - 20), font, 2,
                                     color, 3)
                         # cv2.putText(im_plot, "{:.1f}".format(temp), (box_draw[0], box_draw[1] - 20), font, 1,
                         #             color, 3)
@@ -294,23 +342,17 @@ def main():
                         #         # if l == 0 or l == 3:
                         #         #     color = (0, 255, 0)
                         #         cv2.circle(im, (landmark5[l][0], landmark5[l][1]), 1, color, 1)
-                                # cv2.circle(im, (landmark5[2][0], landmark5[2][1]), 1, color, 2)
-
-
-
-
+                        # cv2.circle(im, (landmark5[2][0], landmark5[2][1]), 1, color, 2)
 
                 cv2.imshow('image', im_plot)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             if g_dumpFiles:
-                frame_name = os.path.join(g_dumpPath, "frame_{}.jpg".format(thermapp.imageId))
+                frame_name = os.path.join(g_dumpPath, "frame_{}.tiff".format(thermapp.imageId))
                 print("save image: {}".format(frame_name))
-                cv2.imwrite(frame_name, im_plot)
+                cv2.imwrite(frame_name, im_raw)
 
-                
-
-            frame_count += 1 # captured frames counter
+            frame_count += 1  # captured frames counter
 
     except KeyboardInterrupt:
         print("Caught ya!")
@@ -320,25 +362,27 @@ def main():
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots(1)
         ax.plot(rate_list, label='Rate[Hz]')
-        avg = sum(rate_list)/len(rate_list)
-        ax.axhline(y = avg, color='r', linestyle='-', label='AVG='+str(round(avg,2)))
-        ax.plot([], color='r', label='Misses='+str(total_misses))
+        avg = sum(rate_list) / len(rate_list)
+        ax.axhline(y=avg, color='r', linestyle='-', label='AVG=' + str(round(avg, 2)))
+        ax.plot([], color='r', label='Misses=' + str(total_misses))
         ax.legend(loc='uppder left')
         ax.set_xlabel('time')
         ax.set_ylabel('rate[Hz]')
         plt.show()
 
-def calib_black_body(im,im_raw, num_of_body=2):
+
+def calib_black_body(im, im_raw, num_of_body=2):
     R = mouse_crop.mouse_crop(image_np=im, image_raw=im_raw, num_of_crop=num_of_body)
-    GL_mean=[]
+    GL_mean = []
     for crop in R[0]:
         GL_mean.append(crop.mean())
 
-    A = (GL_mean[0]-GL_mean[1])/(BLACK_BODY_EMISSIVITY*(BLACK_BODY_TEMP[0]-BLACK_BODY_TEMP[1]))
-    B = GL_mean[0] -BLACK_BODY_EMISSIVITY*A*BLACK_BODY_TEMP[0]
+    A = (GL_mean[0] - GL_mean[1]) / (BLACK_BODY_EMISSIVITY * (BLACK_BODY_TEMP[0] - BLACK_BODY_TEMP[1]))
+    B = GL_mean[0] - BLACK_BODY_EMISSIVITY * A * BLACK_BODY_TEMP[0]
 
-    return A,B
+    coor = R[1]
 
+    return A, B, coor
 
 
 def detect_rectangle(img):
@@ -354,7 +398,7 @@ def detect_rectangle(img):
     # _, contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # opencv 3.x
     # import pdb
     # pdb.set_trace()
-    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # opencv 4.x
+    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # opencv 4.x
 
     # Searching through every region selected to find the required polygon.
     rect_est = None
@@ -365,11 +409,12 @@ def detect_rectangle(img):
         # Shortlisting the regions based on there area.
         if area > 2200 and area < 4200:
 
-            est_polyline = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True), True)  # [top left, bottom left, bottom right, top right]
+            est_polyline = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True),
+                                            True)  # [top left, bottom left, bottom right, top right]
 
             # Checking if the no. of sides of the selected region is 4.
             if (len(est_polyline) == 4):
-            #     cv2.drawContours(img2, [approx], 0, (255, 0,), 5)
+                #     cv2.drawContours(img2, [approx], 0, (255, 0,), 5)
                 rect_est = est_polyline
                 break
 
@@ -382,6 +427,29 @@ def detect_rectangle(img):
         top, left, bottom, right = None, None, None, None
 
     return top, left, bottom, right, rect_est
+
+
+def calc_image_pattern(dirname):
+    image_path_list = os.listdir(dirname)
+
+    image_list = []
+
+    for filename in image_path_list:
+        if filename.endswith(".tiff"):
+            img_path = os.path.join(dirname, filename)
+
+            img = cv2.imread(img_path, -1)
+
+            img = img[:, :, 0]  # take only first channel
+
+            image_list.append(img)
+
+    img_mean = np.stack(image_list, -1).mean(axis=-1)
+
+    img_pattern_mean_subtracted = img_mean - np.mean(img_mean)
+
+    return img_mean, img_pattern_mean_subtracted
+
 
 if __name__ == '__main__':
     main()