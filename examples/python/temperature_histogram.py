c153cde7af550160a501372551a69f980db6fbd0
moshes2 <m@m.com>
2020-04-16 18:32:59 +0300

    wip temperature histogram during work on face tracker

--- examples/python/temperature_histogram.py
+++ examples/python/temperature_histogram.py
@@ -12,7 +12,6 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 from facesIDtracker import facesIdTracker
 
-displayImage = True
 
 class cyclicBuffer(object):
     """
@@ -160,6 +159,9 @@ class TemperatureHistogram(object):
 
         # initialize temperature threshold
         self.temp_th = self.temp_th_nominal
+
+        # initialize face tracker
+        self.faces_tracker = facesIdTracker()
 
 
     def write_sample(self, time_stamp, temp, id=[None]):
@@ -349,29 +351,42 @@ class TemperatureHistogram(object):
         return bins, hist, y_percentage
 
 
-def read_and_display_image(imgpath, filename):
-    global displayImage
+    def write_faces_temperature(self, faces):
+
+        """
+        faces : list
+            Each element in faces is a tuple of (box, temperature)
+            where box is ndarray of [left, top, right, bottom]
+        """
+
+        # track faces ids
+
+        # update faces temprature
+
+
+def read_and_display_image(imgpath, filename, display=False):
 
     if filename.endswith(".png"):
         print(os.path.join(imgpath, filename))
         fig = plt.figure()
         image = mpimg.imread(os.path.join(imgpath, filename))
-        if (displayImage == True) :
+        if display:
             plt.title(filename)
             plt.imshow(image)
             plt.show()
+            plt.pause(0.1)
     return image, int(filename[0:-4])
 
 def detect_faces(image, image_id):
     #On real life Run RetinaDet
     #this is just my debug code:
     if (image_id%3) == 0 :
-        #current_frame_faces is composed of [TL_X, TL_Y, BR_X, BR_y, Temp]
-        current_frame_faces = np.array([[10,20,30,40,37.0], [200, 100, 300, 400, 38], [50, 50, 150, 150, 36]])
+        #current_frame_faces is composed of [(np.array([TL_X, TL_Y, BR_X, BR_y]), Temp)]
+        current_frame_faces = [(np.array([10,20,30,40]), 37.0), (np.array([200, 100, 300, 400]), 38), (np.array([50, 50, 150, 150]), 36)]
     elif (image_id%3) == 1 :
-        current_frame_faces = np.array([[15,20,30,40,37.5], np.array([200,100,300,400, 538])-500 , np.array([50,50,150,150, 16.5])+20] )
+        current_frame_faces = [(np.array([15,20,30,40]),37.5),  (np.array([200,100,300,400])-500, 38.) , (np.array([50,50,150,150])+20, 16.5)]
     else:
-        current_frame_faces = np.array([20,20,30,40,38])
+        current_frame_faces = [(np.array(20,20,30,40), 38)]
 
     return current_frame_faces
 
@@ -384,9 +399,9 @@ def write_element_to_temperature_histogram(current_frame_faces, temperature_hist
     return current_frame_faces
 
 def pop_last_faces_from_histogram(temperature_histogram):
-    last_written_index = temperature_histogram.people_buffer.indWrite
-    last_read_index = temperature_histogram.people_buffer.indRead
-    last_frame_faces = temperature_histogram.people_buffer.buf[(last_read_index):(last_written_index)]
+    last_written_index = temperature_histogram.buffer.indWrite
+    last_read_index = temperature_histogram.buffer.indRead
+    last_frame_faces = temperature_histogram.buffer.buf[(last_read_index):(last_written_index)]
     reshaped_last_frame_faces = last_frame_faces.reshape(-1,6)
     return reshaped_last_frame_faces
 
@@ -419,7 +434,7 @@ def check_for_new_faces_and_update_buffers(image, image_id, current_frame_faces,
     #if there were no faces in last frame write faces as is to buffer
     if len(last_frame_faces_from_buffer) == 0:
         frame_faces_to_buffer = current_frame_faces
-        ids, meanTemp = facesTracker.giveFacesIds(current_frame_faces[:, 0:4], current_frame_faces[:, 4])
+        ids, meanTemp = temperature_histogram.faces_tracker.giveFacesIds(current_frame_faces[:, 0:4], current_frame_faces[:, 4])
         if len(ids) != len(current_frame_faces[:,0]):
             return []
         frame_faces_to_buffer = np.append(current_frame_faces, ids.reshape(ids.shape[0],1), axis=1)
@@ -432,7 +447,7 @@ def check_for_new_faces_and_update_buffers(image, image_id, current_frame_faces,
         lastFrameIds   = last_frame_faces_from_buffer[:, 5]
         currFrameFaces = current_frame_faces[:, 0:4]
         currFrameTemp  = current_frame_faces[:, 4]
-        currFrameIds, meanTemp2 = facesTracker.giveFacesIds(currFrameFaces, currFrameTemp)
+        currFrameIds, meanTemp2 = temperature_histogram.faces_tracker.giveFacesIds(currFrameFaces, currFrameTemp)
         ids_of_new_faces_in_frame = currFrameIds - lastFrameIds
         #maskCurr, maskLast = unq_searchsorted(currFrameIds,lastFrameIds)
         for i,val in enumerate(ids_of_new_faces_in_frame):
@@ -520,6 +535,7 @@ def main_simple_temperature_histogram():
     temp_hist = TemperatureHistogram(hist_calc_interval=hist_calc_interval,
                                      hist_percentile = hist_percentile,
                                      N_samples_for_temp_th=N_samples_for_temp_th,
+                                     N_samples_for_first_temp_th=N_samples_for_first_temp_th,
                                      temp_th_nominal=temp_th_nominal,
                                      buffer_max_len=buffer_max_len,
                                      temp_th_min=temp_th_min,
@@ -562,40 +578,62 @@ def main_simple_temperature_histogram():
 
 def main_temperature_histogram_with_face_tracking():
 
-    time_buffer_max_len = 30 * 60 * 7  # [num_minutes * sec_in_minute * images_per_sec]
+    # time_buffer_max_len = 30 * 60 * 7  # [num_minutes * sec_in_minute * images_per_sec]
+    # temp_th_nominal = 34.0
+    # people_buffer_max_len = 3000
+    # temp_th_min = 30.0
+    # temp_th_max = 36.0
+    #
+    # # ------------------------------
+    # # Images folder to simulate on:
+    # # ------------------------------
+    #
+    # # ------------------------------------
+    # # Temperature histogram initialization
+    # # ------------------------------------
+    # temperature_histogram = TemperatureHistogram(time_buffer_max_len=time_buffer_max_len,
+    #                                              hist_percentile=0.85,
+    #                                              num_people_for_temp_th=50,
+    #                                              num_people_for_first_temp_th=20,
+    #                                              temp_th_nominal=temp_th_nominal,
+    #                                              people_buffer_max_len=people_buffer_max_len,
+    #                                              temp_th_min=temp_th_min,
+    #                                              temp_th_max=temp_th_max,
+    #                                              )
+
+    imgpath = '/home/moshes2/Projects/face_fever_detection/data/2020_30_03__11_09_03/png_im/'
+    display = True
+
+    hist_calc_interval = 30 * 60  # [sec]
+    hist_percentile = 0.85
+    N_samples_for_temp_th = 50
+    N_samples_for_first_temp_th = 20
     temp_th_nominal = 34.0
-    people_buffer_max_len = 3000
+    buffer_max_len = 3000  #
     temp_th_min = 30.0
     temp_th_max = 36.0
 
-    # ------------------------------
-    # Images folder to simulate on:
-    # ------------------------------
-    imgpath = '/home/moshes2/Projects/face_fever_detection/data/2020_30_03__11_09_03/png_im/'
-
-    # ------------------------------------
-    # Temperature histogram initialization
-    # ------------------------------------
-    temperature_histogram = TemperatureHistogram(time_buffer_max_len=time_buffer_max_len,
-                                                 hist_percentile=0.85,
-                                                 num_people_for_temp_th=50,
-                                                 num_people_for_first_temp_th=20,
-                                                 temp_th_nominal=temp_th_nominal,
-                                                 people_buffer_max_len=people_buffer_max_len,
-                                                 temp_th_min=temp_th_min,
-                                                 temp_th_max=temp_th_max,
-                                                 )
-
-    facesTracker = facesIdTracker()
+    temp_hist = TemperatureHistogram(hist_calc_interval=hist_calc_interval,
+                                     hist_percentile = hist_percentile,
+                                     N_samples_for_temp_th=N_samples_for_temp_th,
+                                     N_samples_for_first_temp_th=N_samples_for_first_temp_th,
+                                     temp_th_nominal=temp_th_nominal,
+                                     buffer_max_len=buffer_max_len,
+                                     temp_th_min=temp_th_min,
+                                     temp_th_max=temp_th_max,
+                                     )
 
     # ------------------------------
     #           Main loop:
     # ------------------------------
     for filename in sorted(os.listdir(imgpath)):
 
-        image, image_id = read_and_display_image(imgpath, filename)
+        image, image_id = read_and_display_image(imgpath, filename, display)
+
         current_frame_faces = detect_faces(image, image_id)
-        new_faces_from_buffer = check_for_new_faces_and_update_buffers(image, image_id,current_frame_faces, temperature_histogram, temp_th_nominal)
+
+        new_faces_from_buffer = check_for_new_faces_and_update_buffers(image, image_id, current_frame_faces, temp_hist, temp_th_nominal)
+
         display_new_people_on_image(image, image_id, new_faces_from_buffer)
 
 
@@ -603,9 +641,9 @@ def main_temperature_histogram_with_face_tracking():
 
 if __name__ == '__main__':
 
-    main_simple_temperature_histogram()
+    # main_simple_temperature_histogram()
 
-    # main_temperature_histogram_with_face_tracking()
+    main_temperature_histogram_with_face_tracking()
 
     print ('Done')
 
